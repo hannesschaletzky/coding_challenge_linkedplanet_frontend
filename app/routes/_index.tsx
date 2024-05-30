978ee7fbeadabb28eb4d554c92fd0cdbf140bc3b
Hannes Schaletzky <hannes.schaletzky@gmx.de>
2024-05-31 01:28:36 +0200

    display signal chain with values from db done

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -11,8 +11,8 @@ export const meta: MetaFunction = () => {
   return [
     { title: "Device Signal Chain" },
     {
-      name: "An overview of your event broadcasting",
-      content: "Welcome to the app!",
+      name: "An overview of your event broadcast",
+      content: "🏐",
     },
   ];
 };
@@ -51,40 +51,44 @@ export default function Index() {
   const loaderData = useLoaderData<typeof loader>();
   console.log("devices:", loaderData.devices.length);
   console.log("connections:", loaderData.connections.length);
+  console.log("connections:", loaderData.connections);
 
   useEffect(() => {
-    const nodes = [
-      { id: 0, label: "0" },
-      { id: 1, label: "1" },
-      { id: 2, label: "2" },
-      { id: 3, label: "3" },
-      { id: 4, label: "4" },
-      { id: 5, label: "5" },
-      { id: 6, label: "6" },
-    ];
+    const allConnectionDeviceNames: string[] = loaderData.connections.flatMap(
+      (connection) => [
+        connection.source_device_name,
+        connection.target_device_name,
+      ]
+    );
+    const uniqueDeviceNamesSet: Set<string> = new Set(allConnectionDeviceNames);
+    const uniqueDeviceNames: string[] = Array.from(uniqueDeviceNamesSet);
 
-    const edges = [
-      { from: 0, to: 4 },
-      { from: 1, to: 4 },
-      { from: 2, to: 4 },
-      { from: 3, to: 5 },
-      { from: 4, to: 6 },
-      { from: 5, to: 6 },
-    ];
+    const nodes = uniqueDeviceNames.map((device) => {
+      return { id: device, label: device };
+    });
+
+    const edges = loaderData.connections.map((con) => {
+      return { from: con.source_device_name, to: con.target_device_name };
+    });
 
     const container = document.getElementById("mynetwork");
     if (container == null) {
       throw new Error("#mynetwork could not be found");
     }
+
     const data = {
       nodes: nodes,
       edges: edges,
     };
+
     // see options here: https://visjs.github.io/vis-network/docs/network/index.html
     const options = {
       nodes: {
         shape: "dot",
         size: 20,
+        font: {
+          size: 18,
+        },
       },
       edges: {
         arrows: "to",
@@ -98,8 +102,9 @@ export default function Index() {
         },
       },
     };
+
     new Network(container, data, options);
-  }, []);
+  }, [loaderData.connections]);
 
   return (
     <>
